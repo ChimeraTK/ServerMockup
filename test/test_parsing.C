368a884663140087188eebf8edd04c938551a69d
Zenker, Dr. Klaus (FWKE) - 126506 <k.zenker@hzdr.de>
2018-04-16 09:50:20 +0200

    Fix input file name.

--- test/test_parsing.C
+++ test/test_parsing.C
@@ -7,7 +7,7 @@
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE sysTest
 
-#include "XMLParser.h"
+#include "ServerMockup.h"
 
 #include <boost/test/unit_test.hpp>
 using namespace boost::unit_test_framework;
@@ -15,7 +15,7 @@ using namespace boost::unit_test_framework;
 using namespace std;
 
 BOOST_AUTO_TEST_CASE( testXMLParsing){
-  xml_parser::XMLParser parser("ServerMockup_input.xml");
+  xml_parser::XMLParser parser(INPUT);
   BOOST_CHECK_NO_THROW(parser.parse());
   for(auto &var: parser.vars){
     var.print();
